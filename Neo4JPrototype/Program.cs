abb3707363a075189f65e312cef0753d03ecf446
Steven Suing <Steven.suing@gmail.com>
2013-06-01 12:36:37 -0400

    Updated with ShunLan Example

--- Neo4JPrototype/Program.cs
+++ Neo4JPrototype/Program.cs
@@ -19,23 +19,65 @@ namespace Neo4JPrototype
             // Init
             Connect();
 
-      //      ClearDatabase();
+            ClearDatabase();
 
-         //   CreateSampleData();
+            //CreateSampleData();
+
+            CreateSampleDataShunLan();
 
             ExecuteQueries();
 
+            ////START n=node(*) 
+            ////MATCH n-[r?]-() 
+            ////RETURN n, r;
 
         }
 
         private static void ExecuteQueries()
         {
-            var myNode = client.Cypher
-                               .Start(new {n = Node.ByIndexQuery("Name", "Joseph")})
-                               .Return<Person>("n")
-                               .Results.First();
+            //Make a query to get ShunLan's node
+            var ShunLanNode = client.Cypher
+                               .Start(new { n = Node.ByIndexLookup("node_auto_index", "Name", "ShunLan") });
 
-            Console.WriteLine(myNode.Name);
+            var ShunLanNodeResult = ShunLanNode.Return<Person>("n");
+
+
+            Console.WriteLine("Returning a single node : {0}", ShunLanNodeResult.Results.First().Name);
+
+            //Return the number of nodes
+            var allNodes = client.Cypher
+                               .Start(new { all = All.Nodes })
+                               .Return<Object>("all")
+                               .Results;
+
+            Console.WriteLine("There are {0} nodes", allNodes.Count());
+
+            //Return what shun lan Owns
+            var allShunLan = ShunLanNode
+                           .Match("n-[:OWNER]-> property")
+                       .Return<Property>("property")
+                       .Results.ToList();
+            Console.WriteLine("Shun-Lan Owns {0} things", allShunLan.Count());
+
+            //This shows the literal serializer. 
+            allShunLan.ForEach(c => Console.WriteLine("      {0}", c.Name()));
+
+            //Return vehicles shun lan Owns
+            var allShunLanVeh = ShunLanNode
+                         .Match("n-[:OWNER]-> property")
+                         .Where("property.NodeTypeSub = \"VEHICLE\"")
+                     .Return<Vehicle>("property")
+                     .Results.ToList();
+            Console.WriteLine("Shun-Lan Owns {0} Vehicles {1}", allShunLanVeh.Count(), allShunLanVeh[0].MakeModel);
+
+            //Return Homes shun lan Owns
+            var allShunLanHomes = ShunLanNode
+                         .Match("n-[:OWNER]-> property")
+                         .Where("property.NodeTypeSub = \"HOME\"")
+                     .Return<Home>("property")
+                     .Results.ToList();
+            Console.WriteLine("Shun-Lan Owns {0} Home {1}", allShunLanHomes.Count(), allShunLanHomes[0].HomeAddress);
+
 
             // Should Joesph get marketing about home insurance?  No Wife already has it
 
@@ -50,6 +92,81 @@ namespace Neo4JPrototype
             // Show me everything Joseph is related to 2 levels deep
 
             // What happens when Joseph divorces Ann.
+
+
+        }
+
+        private static void CreateSampleDataShunLan()
+        {
+
+            // Create entities
+            var ShunLan = client.Create(new Person() { Name = "ShunLan" });
+            var BillyHub = client.Create(new Person() { Name = "BillyHub" });
+            var BobbySon = client.Create(new Person() { Name = "BobbySon" });
+
+            var autoPolicyA = client.Create(new Policy() { PolicyNumber = "AutoPolicy A" });
+            // var cyclePolicyA = client.Create(new Policy() { PolicyNumber = "Cycle Policy B" });
+            var homePolicyA = client.Create(new Policy() { PolicyNumber = "Home Policy A" });
+
+            var veh1 = client.Create(new Vehicle() { MakeModel = "Honda 1", Id = "111" });
+            var veh2 = client.Create(new Vehicle() { MakeModel = "Subaru 2", Id = "222" });
+            var veh3 = client.Create(new Vehicle() { MakeModel = "MG 3", Id = "333" });
+
+            var home = client.Create(new Home() { HomeAddress = "1 GEICO Plaza", Id = "12345" });
+
+
+            // ShunLan is Spouse of BillyHub
+            client.CreateRelationship(ShunLan, new IsSpouseRelationship(BillyHub));
+
+            // BobbySon child of ShunLan 
+            client.CreateRelationship(BobbySon, new IsChildRelationship(ShunLan));
+
+            // BobbySon child of BillyHub
+            client.CreateRelationship(BobbySon, new IsChildRelationship(BillyHub));
+
+            // ShunLan is NIN on Auto Policy A
+            client.CreateRelationship(ShunLan, new NamedInsuredRelationship(autoPolicyA, new NamedInsuredData("Named Insured Data")));
+
+            // BillyHub is SIN on Auto Policy A
+            client.CreateRelationship(BillyHub, new SecondaryInsuredRelationship(autoPolicyA));
+
+            //PolicyA has 3 Operators.  THis doesn't mean they actually drive all the cars. 
+            client.CreateRelationship(ShunLan, new OperatorRelationship(autoPolicyA));
+            client.CreateRelationship(BillyHub, new OperatorRelationship(autoPolicyA));
+            client.CreateRelationship(BobbySon, new OperatorRelationship(autoPolicyA));
+
+            //BillyHub  Owns Veh 1 and Veh 3
+            client.CreateRelationship(BillyHub, new OwnerRelationship(veh1));
+            client.CreateRelationship(BillyHub, new OwnerRelationship(veh3));
+
+            //ShunLan Owns Veh 2
+            client.CreateRelationship(ShunLan, new OwnerRelationship(veh2));
+
+            //BillyHub Drives Vehicle 1
+            client.CreateRelationship(BillyHub, new PolicyDriverRelationship(veh1));
+
+            //ShunLan Drives Veh 2
+            client.CreateRelationship(ShunLan, new PolicyDriverRelationship(veh2));
+
+            //BobbySon Drives Veh 3
+            client.CreateRelationship(BobbySon, new PolicyDriverRelationship(veh3));
+
+            //ShunLan Owns Home
+            client.CreateRelationship(ShunLan, new OwnerRelationship(home));
+
+            //BillyHub Owns Home as well
+            client.CreateRelationship(BillyHub, new OwnerRelationship(home));
+
+            // ShunLan Has a home Policy
+            client.CreateRelationship(ShunLan, new NamedInsuredRelationship(homePolicyA, new NamedInsuredData("sdajklf")));
+
+            // PolicyA insures Veh1, Veh2, and Veh3
+            client.CreateRelationship(autoPolicyA, new InsuresRelationship(veh1));
+            client.CreateRelationship(autoPolicyA, new InsuresRelationship(veh2));
+            client.CreateRelationship(autoPolicyA, new InsuresRelationship(veh3));
+
+            // HomePolicy insures Home
+            client.CreateRelationship(homePolicyA, new InsuresRelationship(home));
 
 
         }
@@ -85,10 +202,6 @@ namespace Neo4JPrototype
 
             // Joesph gets cycle policy and is only driver on it NIN on Policy A
             client.CreateRelationship(joseph, new NamedInsuredRelationship(cyclePolicyA, new NamedInsuredData("sdajklf")));
-
-
-
-
         }
 
         private static void Connect()
@@ -141,6 +254,55 @@ namespace Neo4JPrototype
         }
     }
 
+    public class Property
+    {
+        public static readonly string NodeTypeKey = "PROPERTY";
+
+        public string NodeType
+        {
+            get { return NodeTypeKey; }
+        }
+
+        public virtual string Name()
+        {
+            return "No Name given";
+        }
+    }
+
+    public class Vehicle : Property
+    {
+        public static readonly string NodeTypeSubKey = "VEHICLE";
+        public string Id { get; set; }
+        public string MakeModel { get; set; }
+
+        public string NodeTypeSub
+        {
+            get { return NodeTypeSubKey; }
+        }
+        public override string Name()
+        {
+            return string.Format("Vehicle: {0}", MakeModel);
+        }
+    }
+
+    public class Home : Property
+    {
+        public static readonly string NodeTypeSubKey = "HOME";
+
+        public string Id { get; set; }
+        public string HomeAddress { get; set; }
+
+        public string NodeTypeSub
+        {
+            get { return NodeTypeSubKey; }
+        }
+
+        public override string Name()
+        {
+            return string.Format("Home: {0}", HomeAddress);
+        }
+    }
+
     #endregion
 
 
@@ -179,6 +341,41 @@ namespace Neo4JPrototype
             get { return TypeKey; }
         }
     }
+
+    public class OperatorRelationship : Relationship, IRelationshipAllowingSourceNode<Person>,
+                                        IRelationshipAllowingTargetNode<Person>
+    {
+        public static readonly string TypeKey = "OPERATOR";
+
+        public OperatorRelationship(NodeReference targetNode)
+            : base(targetNode)
+        {
+        }
+
+        public override string RelationshipTypeKey
+        {
+            get { return TypeKey; }
+        }
+    }
+
+
+    public class OwnerRelationship : Relationship, IRelationshipAllowingSourceNode<Person>,
+                                    IRelationshipAllowingTargetNode<Vehicle>
+    {
+        public static readonly string TypeKey = "OWNER";
+
+        public OwnerRelationship(NodeReference targetNode)
+            : base(targetNode)
+        {
+        }
+
+        public override string RelationshipTypeKey
+        {
+            get { return TypeKey; }
+        }
+    }
+
+
     public class PolicyDriverRelationship : Relationship, IRelationshipAllowingSourceNode<Person>,
                                        IRelationshipAllowingTargetNode<Person>
     {
@@ -194,6 +391,23 @@ namespace Neo4JPrototype
             get { return TypeKey; }
         }
     }
+
+    public class IsSpouseRelationship : Relationship, IRelationshipAllowingSourceNode<Person>,
+                                   IRelationshipAllowingTargetNode<Person>
+    {
+        public static readonly string TypeKey = "ISSPOUSE";
+
+        public IsSpouseRelationship(NodeReference targetNode)
+            : base(targetNode)
+        {
+        }
+
+        public override string RelationshipTypeKey
+        {
+            get { return TypeKey; }
+        }
+    }
+
     public class IsChildRelationship : Relationship, IRelationshipAllowingSourceNode<Person>,
                                        IRelationshipAllowingTargetNode<Person>
     {
@@ -209,6 +423,22 @@ namespace Neo4JPrototype
             get { return TypeKey; }
         }
     }
+    public class InsuresRelationship : Relationship, IRelationshipAllowingSourceNode<Policy>,
+                                    IRelationshipAllowingTargetNode<Vehicle>
+    {
+        public static readonly string TypeKey = "INSURES";
+
+        public InsuresRelationship(NodeReference targetNode)
+            : base(targetNode)
+        {
+        }
+
+        public override string RelationshipTypeKey
+        {
+            get { return TypeKey; }
+        }
+    }
+
     public class IsWifeRelationship : Relationship, IRelationshipAllowingSourceNode<Person>,
                                     IRelationshipAllowingTargetNode<Person>
     {
